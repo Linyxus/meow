b7dfd1fe06126789e77ed1f68977552de55b36fc
Yichen Xu <yichen.x@outlook.com>
2020-12-17 22:18:03 +0800

    Add Semigroup

--- src/main/scala/meow/Semigroup.scala
+++ src/main/scala/meow/Semigroup.scala
@@ -3,9 +3,3 @@ package meow
 trait Semigroup[A] {
   def mplus(l: A, r: A): A
 }
-
-trait SemigroupInstances {
-  implicit def listIsSemigroup[A]: Semigroup[List[A]] = new Semigroup[List[A]] {
-    override def mplus(l: List[A], r: List[A]): List[A] = l ++ r
-  }
-}
