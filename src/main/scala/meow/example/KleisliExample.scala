30fa813883e791bbffef632ecb3a2f46339c1c4e
Yichen Xu <yichen.x@outlook.com>
2020-12-18 14:33:03 +0800

    Reduce type hint boilerplate in Kleisli example

--- src/main/scala/meow/example/KleisliExample.scala
+++ src/main/scala/meow/example/KleisliExample.scala
@@ -6,11 +6,12 @@ import monad.Writer
 import Writer._
 
 object KleisliExample extends App {
-  def liftK[A, B](func: A => B): Kleisli[Writer[List[String], *], A, B] = Kleisli { x =>
+  type App[A, B] = Kleisli[Writer[List[String], *], A, B]
+  def liftK[A, B](func: A => B): App[A, B] = Kleisli { x =>
     val y = func(x)
     tell(s"change from $x to $y".pure[List]) >> y.pure[Writer[List[String], *]]
   }
-  val app: Kleisli[Writer[List[String], *], Int, Int] =
-    (liftK[Int, Int](_ * 2) &&& liftK[Int, Int](_ + 1)) >>> liftK[(Int, Int), Int] { case (a, b) => a * b }
+  val app: App[Int, Int] =
+    (liftK[Int, Int](_ * 2) &&& liftK(_ + 1)) >>> liftK { case (a, b) => a * b }
   println(app.runKleisli(10).run)
 }
