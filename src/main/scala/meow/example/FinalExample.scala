2c25a5f7ee84aea50c0ff5adfa9dfa15c3785482
Yichen Xu <yichen.x@outlook.com>
2021-01-02 00:40:29 +0800

    Add pushNeg in final interpreter

--- src/main/scala/meow/example/FinalExample.scala
+++ src/main/scala/meow/example/FinalExample.scala
@@ -34,6 +34,35 @@ object FinalExample extends App {
 
   def view(e: ForallBounded[ExpSymb]): String = e.apply[String]
 
+  trait Ctx
+  case object PosCtx extends Ctx
+  case object NegCtx extends Ctx
+
+  implicit def expCanPushNeg[A : ExpSymb]: ExpSymb[Ctx => A] = new ExpSymb[Ctx => A] {
+    val fa: ExpSymb[A] = implicitly
+
+    override def lit(i: Int): Ctx => A = {
+      case NegCtx => fa.neg(fa.lit(i))
+      case PosCtx => fa.lit(i)
+    }
+
+    override def neg(a: Ctx => A): Ctx => A = {
+      case NegCtx => a(PosCtx)
+      case PosCtx => a(NegCtx)
+    }
+
+    override def add(a: Ctx => A, b: Ctx => A): Ctx => A = ctx => fa.add(a(ctx), b(ctx))
+
+    override def mul(a: Ctx => A, b: Ctx => A): Ctx => A = ctx => fa.mul(a(ctx), b(ctx))
+  }
+
+  val pushNeg: ForallBounded[ExpSymb] => ForallBounded[ExpSymb] = x => new ForallBounded[ExpSymb] {
+    override def apply[A: ExpSymb]: A = {
+      val func = x.apply[Ctx => A]
+      func(PosCtx)
+    }
+  }
+
   val prog1: ForallBounded[ExpSymb] = new ForallBounded[ExpSymb] {
     override def apply[A: ExpSymb]: A = {
       val f: ExpSymb[A] = implicitly
@@ -66,4 +95,9 @@ object FinalExample extends App {
 
   println(view(prog2))
   println(eval(prog2))
+
+  val prog3: ExpProg = add(neg(neg(lit(1))), neg(lit(2)))
+  val prog4: ExpProg = pushNeg(prog3)
+  println(view(prog3))
+  println(view(prog4))
 }
