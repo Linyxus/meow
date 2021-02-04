6b4e22332c612531bd38ec7ff23a1b0a91e53186
Yichen Xu <yichen.x@outlook.com>
2021-02-04 14:21:57 +0800

    Reformat TTFdBExample

--- src/main/scala/meow/example/TTFdBExample.scala
+++ src/main/scala/meow/example/TTFdBExample.scala
@@ -5,7 +5,7 @@ import TTFdB._
 
 object TTFdBExample extends App {
   // 1 + 2
-  def prog1[Repr[_, _] : Symantics, Env]: Repr[Env, Int] = {
+  def prog1[Repr[_, _]: Symantics, Env]: Repr[Env, Int] = {
     val f: Symantics[Repr] = implicitly
     f.add(f.int(1), f.int(2))
   }
@@ -14,7 +14,7 @@ object TTFdBExample extends App {
   println(eval[Unit, Int](prog1[Eval, Unit], ()))
 
   // (\x. 1 + x) 2
-  def prog2[Repr[_, _] : Symantics, Env]: Repr[Env, Int] = {
+  def prog2[Repr[_, _]: Symantics, Env]: Repr[Env, Int] = {
     val f: Symantics[Repr] = implicitly
     f.app(f.lam(f.add(f.int[(Int, Env)](1), f.here[Env, Int])), f.int(2))
   }
